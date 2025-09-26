b42b491f117cadaf847308f7ba5de051003132fa
default <ketia.ineza@stu.ocsb.ca>
2025-09-26 15:12:20 +0000

    Added program to display my name

---
+++ main.ts
@@ -0,0 +1,11 @@
+/**
+* @author Ketia Ineza
+* @version 1.0.0
+* @date 2025-09-26
+* @fileoverview create program to display my first and last name
+*/
+
+// create program to display my first and last name
+console.log("Ketia Ineza");
+
+console.log("\nDone.");
